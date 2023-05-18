51a70980f545c9ef31628d1d7bd0c2ad62c0e9f7
shixiuhai <15256728901@163.com>
2023-05-19 00:09:45 +0800

    更新了评论表

--- mall-tiny/src/main/java/com/walk/mall/tiny/modules/ums/controller/UmsBusinessWorkcommentController.java
+++ mall-tiny/src/main/java/com/walk/mall/tiny/modules/ums/controller/UmsBusinessWorkcommentController.java
@@ -1,8 +1,10 @@
 package com.walk.mall.tiny.modules.ums.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,7 +43,24 @@ public class UmsBusinessWorkcommentController {
                 @RequestParam(value = "type", required = false) Integer type){
         Page<UmsBusinessWorkcomment> all = umsBusinessWorkcommentService.list(workId, anchorId, userId, page, size, type);
         return CommonResult.success(CommonPage.restPage(all));
-    
+    }
+
+    @PutMapping("")
+    public CommonResult update(@RequestBody UmsBusinessWorkcomment umsBusinessWorkcomment){
+        boolean success = umsBusinessWorkcommentService.updateById(umsBusinessWorkcomment);
+        if(success){
+            return CommonResult.success(null, null);
+        }
+        return CommonResult.failed();
+    }
+
+    @DeleteMapping("")
+    public CommonResult<Object> delete(@RequestParam(value = "id", required = true) Integer id){
+        boolean success = umsBusinessWorkcommentService.removeById(id);
+        if(success){
+            return CommonResult.success(null, null);
+        }
+        return CommonResult.failed();
     }
 
     
