998ed5db972ec1e19d3430435a60de8b62ccf27a
shixiuhai <15256728901@163.com>
2023-05-18 21:58:43 +0800

    实现了微信小程序登陆但是没有测试

--- wx/src/main/java/com/walk/wx/service/impl/AuthServiceImpl.java
+++ wx/src/main/java/com/walk/wx/service/impl/AuthServiceImpl.java
@@ -3,7 +3,12 @@ package com.walk.wx.service.impl;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+
 import java.util.Map;
+import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,6 +19,8 @@ import com.walk.wx.service.AuthService;
 import com.walk.wx.service.WxMiniApi;
 import com.walk.wx.service.impl.WxMiniApiImpl;
 import com.walk.mall.tiny.common.api.CommonResult;
+import com.walk.mall.tiny.modules.ums.dto.UmsAdminParam;
+import com.walk.mall.tiny.modules.ums.model.UmsAdmin;
 import com.walk.mall.tiny.modules.ums.service.UmsAdminService;
 import com.walk.wx.utils.WeChatUtil;
 import lombok.extern.slf4j.Slf4j;
@@ -56,27 +63,56 @@ public class AuthServiceImpl implements AuthService {
         if(jsonObject == null) {
             throw new RuntimeException("调用微信端授权认证接口错误");
         }
-        String openId = jsonObject.getString("openid");
+        // String openId = jsonObject.getString("openid");
         String sessionKey = jsonObject.getString("session_key");
-        String unionId = jsonObject.getString("unionid");
+        // String unionId = jsonObject.getString("unionid");
         
-        String result = WeChatUtil.decryptData(unionId, sessionKey, iv);
+        // 解析出的微信加密结果
+        String result = WeChatUtil.decryptData(encryptedData, sessionKey, iv);
         log.info("--------------------------------------");
         log.info("输出的结果是{}",result);
         log.info("--------------------------------------");
-        Map<String,String> wxUser = new HashMap<String,String>();
-        // Map<String,String> wxUser = new HashMap<>();
+        
+        // 创建一个返回对象
+        // Map<String,String> wxUser = new HashMap<String,String>();
+        Map<String,String> wxUser = new HashMap<>();
         // wxUser.put("openId", openId);
         // wxUser.put("sessionKey",sessionKey);
         // wxUser.put("unionId",unionId);
         
+        // 将结果转换为json数据
+        JSONObject wechatResult = JSONObject.parseObject(result) ;
+        // 获取电话号码
+        String phoneNumber = wechatResult.getString("phoneNumber");
+        // 在数据库查询是否存在用户
+        UmsAdmin  umsAdmin = umsAdminService.getOne(new LambdaQueryWrapper<UmsAdmin>()
+            .eq(!StringUtils.isEmpty(phoneNumber),UmsAdmin::getUsername , phoneNumber));
+        
+        // 用户存在直接登陆
+        if(Objects.nonNull(umsAdmin)){
+            String token = umsAdminService.login(phoneNumber, password);
+            // 添加信息到wxUser中
+            wxUser.put("token", token);
+        }
 
+        // 用户不存在创建用户登陆
+        UmsAdminParam umsAdminParam = new UmsAdminParam();
+        umsAdminParam.setUsername(phoneNumber);
+        umsAdminParam.setPassword(password);
+
+        umsAdmin = umsAdminService.register(umsAdminParam);
+        String token = umsAdminService.login(umsAdmin.getUsername(),password);
+        wxUser.put("token", token);
+        // 返回微信登陆用户信息
+        return wxUser;
+        
+        
         // 以下是我们生产环境正在使用的流程，供您参考一下下[呲牙]
         // 1. 项目初始化时调用`Taro.login`接口获取到code
         // 2. 将`code`发送给后端 后端使用code换取用户`sessionKey`，并保存起来。
         // 3. 用户点击按钮拉起`获取手机号授权框`并确认授权后，将`iv`、`encryptedData`提交给后端
         // 4. 后端使用 `iv` + `encryptedData` + `sessionKey`可以解出手机号并脱敏返回给前端。 自此，手机号授权流程结束。
-        return wxUser;
+        
     }
    
 }
