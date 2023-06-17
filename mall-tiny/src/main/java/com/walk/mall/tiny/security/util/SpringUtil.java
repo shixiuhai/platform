ee84d08d673974ca8811b64fa4461b440369e472
shixiuhai <15256728901@163.com>
2023-06-18 02:17:22 +0800

    删除了不必要的内容

--- mall-tiny/src/main/java/com/walk/mall/tiny/security/util/SpringUtil.java
+++ mall-tiny/src/main/java/com/walk/mall/tiny/security/util/SpringUtil.java
@@ -6,7 +6,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
-import com.walk.mall.tiny.modules.ums.model.UmsAdmin;
 import com.walk.mall.tiny.domain.AdminUserDetails;
 
 
