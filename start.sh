5944ad5a84afc68c38e541973f4b05e981ea8bcc
shixiuhai <15256728901@163.com>
2023-06-18 23:28:50 +0800

    更新了启动文件

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
-java -jar .\main-1.1.2.jar --spring.profiles.active=mallprod,wxLogin
+java -jar ./main-1.1.2.jar --spring.profiles.active=mallprod,wxLogin
+nohup java -jar ./main-1.1.2.jar > info.log 2>&1 &
 # java -jar .\main-1.1.2.jar --spring.profiles.active=malldev,wxLogin
